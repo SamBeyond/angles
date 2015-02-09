b530e7aaa6cb2a4b53fc4046e557898e71098a40
Sam Beyond <sam.beyond@octopost.eu>
2015-02-09 17:26:37 +0100

    crawl retweets and new explorers

--- src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
+++ src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
@@ -25,35 +25,6 @@ import twitter4j.TwitterException
 import scala.collection.mutable.Queue
 
 object UpdateStoryIndex extends App {
-  val log = LoggerFactory.getLogger(UpdateStoryIndex.getClass)
-
-  val work = new Queue[Step]()
-  work.enqueue(new FetchTimelines)
-  work.enqueue(new FetchRetweets)
-  work.enqueue(new FetchExplorers)
-  work.enqueue(new CrawlUris)
-  work.enqueue(new ExtractMetadata)
-  work.enqueue(new ExtractNamedEntities)
-  work.enqueue(new IndexArticles)
-
   val since = new DateTime().minusDays(5)
-  do {
-    try {
-      val step = work.dequeue()
-      step.execute(since)
-    } catch {
-      case tex: TwitterException => {
-        if (tex.exceededRateLimitation()) {
-          val limit = tex.getRateLimitStatus
-
-          val sleepTime = limit.getSecondsUntilReset + 1
-          log.info("Rate limit exceeded, going to sleep for {} seconds and then try again", sleepTime)
-          Thread.sleep(sleepTime * 1000)
-        } else {
-          // Ugh?!
-          log.error("{}", tex)
-        }
-      }
-    }
-  }  while (!work.isEmpty)
+  new IndexArticles().execute(since)
 }
